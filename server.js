8269469bcfedced3af04a98be40c1fb7e5de5eb4
parv66 <82944668+parv66@users.noreply.github.com>
2022-06-16 19:21:28 +0530

    basic stuff
    basic stuff lol

---
+++ server.js
@@ -0,0 +1,16 @@
+const { Client } = require("discord.js"); //makes process easy
+const client = new Client({ //makes it so bot runs everywhere(for testing)
+  intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"],
+  partials: ["CHANNEL"]
+});
+client.on("ready", () => {
+  console.log("bot is running"); //when bot is ready
+});
+client.on("messageCreate", async message => {
+  // when client detects a message
+  if (message.author.bot) return; // if the author of the message is a bot ignore the case
+  if (!message.content) //when images and stuff get posted
+    return message.reply({
+      content: "invalid number" //makes the bot reply
+    });
+client.login(process.env.TOKEN); //login using token which doent exists yet
