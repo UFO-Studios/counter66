305fbe66253aa946b11f8f6f7da3a232c57274e5
parv66 <82944668+parv66@users.noreply.github.com>
2022-06-16 23:40:48 +0530

    read from config.json

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 let Discord = require("discord.js")
 const client = new Discord.Client()
-const { botToken } = require('./config.json')
+const { data } = require('./config.json')
 
 client.on("ready", async () => {
   console.log("Running!")
 });
 
 client.on("message", async message => {
-  if (message.channel.id === "953731286659383349"){
-  if (message.author.bot) return;
-  if (message.content) {
-    var e = parseInt(message.content) + 1;
-    if (isNaN(parseFloat(e))) {
-      return message.reply('you ruined it! Start at 0 again');
+  if (message.channel.id === ${data.channel}){
+    if (message.author.bot) return;
+    if (message.content) {
+      var e = parseInt(message.content) + 1;
+      if (isNaN(parseFloat(e))) {
+        return message.reply('you ruined it! Start at 0 again');
+      }
+      else if (e !== NaN) {
+        return message.channel.send(e);
+      }
     }
-    else if (e !== NaN) {
-      return message.channel.send(e);
-    }
-  }}
+  }
 })
 
-
-client.login(process.env.token);
+client.login(process.data.botToken);
