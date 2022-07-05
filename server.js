7dc28a6f9073ee573fc32fa04539b1c910833016
nceygy01 <>
2022-07-05 19:19:47 +0100

    test

--- server.js
+++ server.js
@@ -1,24 +1,21 @@
 let Discord = require("discord.js")
-const client = new Discord.Client()
+//const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Discord.Client({ intents: [ 'DIRECT_MESSAGES', 'GUILD_MESSAGES' ] });
 const { data } = require('./config.json')
+
 
 client.on("ready", async () => {
   console.log("Running!")
 });
 
 client.on("message", async message => {
-  if (message.channel.id === ${data.channel}){
-    if (message.author.bot) return;
-    if (message.content) {
-      var e = parseInt(message.content) + 1;
-      if (isNaN(parseFloat(e))) {
-        return message.reply('you ruined it! Start at 0 again');
-      }
-      else if (e !== NaN) {
-        return message.channel.send(e);
-      }
-    }
-  }
-})
+    message.channel.send('Pong!')
+  })
+
+
+ command(client, ['ping', 'test'], (message) => {
+    message.channel.send('Pong!')
+  })
+
 
 client.login(process.data.botToken);
